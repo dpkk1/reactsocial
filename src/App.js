89a43bef0bad2730c84b0987547492a4d1e8be44
DEEPAK KUMAR <deepakkumar7228488@gmail.com>
2021-07-12 08:55:36 +0530

    Add the Facebook Authentication project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Facebook from './components/Facebook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Facebook Auth Example</h1>
       </header>
+      <p className="App-intro">To get Started, Authenticate with Facebook.</p>
+      <Facebook />
     </div>
   );
 }
